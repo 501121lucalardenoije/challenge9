e32d3c4a0e75aee5ea43fa4a18d32d3149651e97
Raphael Pedro <503978@vistacollege.nl>
2021-04-14 13:52:51 +0200

    Update contact.php

--- contact.php
+++ contact.php
@@ -24,7 +24,7 @@ require_once("assets/languages/lang.$lang.php")
     <meta name="author" content="">
     <link href="https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap" rel="stylesheet">
 
-    <title>V!st@ CARS</title>
+    <title>Buurtzorg</title>
     <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
 
     <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.css">
@@ -34,20 +34,7 @@ require_once("assets/languages/lang.$lang.php")
     </head>
     
     <body>
-    
-    <!-- ***** Preloader Start ***** -->
-    <div id="js-preloader" class="js-preloader">
-      <div class="preloader-inner">
-        <span class="dot"></span>
-        <div class="dots">
-          <span></span>
-          <span></span>
-          <span></span>
-        </div>
-      </div>
-    </div>
-    <!-- ***** Preloader End ***** -->
-    
+
     
     <!-- ***** Header Area Start ***** -->
     <?php include_once("assets/includes/Header.Area.php");?>
@@ -116,49 +103,49 @@ require_once("assets/languages/lang.$lang.php")
    
     <!-- ***** Contact Us Area Starts ***** -->
     <section class="section" id="contact-us" style="margin-top: 0">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-lg-6 col-md-6 col-xs-12">
-                    <div id="map">
-                      <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d10076.769477197302!2d5.7174463!3d50.8461219!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x952ffbf18f188c5c!2sVISTA%20college!5e0!3m2!1snl!2snl!4v1611915318875!5m2!1snl!2snl" width="100%" height="600px" frameborder="0" style="border:0" allowfullscreen></iframe>
-                    </div>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-6 col-md-6 col-xs-12">
+                <div id="map">
+                  <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d10076.769477197302!2d5.7174463!3d50.8461219!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x952ffbf18f188c5c!2sVISTA%20college!5e0!3m2!1snl!2snl!4v1611915318875!5m2!1snl!2snl" width="100%" height="600px" frameborder="0" style="border:0" allowfullscreen></iframe>
                 </div>
-                <div class="col-lg-6 col-md-6 col-xs-12">
-                    <div class="contact-form section-bg" style="background-image: url(assets/images/DOCWR.jpg)">
-                        <form id="contact" action="" method="post">
-                          <div class="row">
-                            <div class="col-md-6 col-sm-12">
-                              <fieldset>
-                                <input name="name" type="text" id="name" placeholder="Your Name*" required="">
-                              </fieldset>
-                            </div>
-                            <div class="col-md-6 col-sm-12">
-                              <fieldset>
-                                <input name="email" type="text" id="email" pattern="[^ @]*@[^ @]*" placeholder="Your Email*" required="">
-                              </fieldset>
-                            </div>
-                            <div class="col-md-12 col-sm-12">
-                              <fieldset>
-                                <input name="subject" type="text" id="subject" placeholder="Subject">
-                              </fieldset>
-                            </div>
-                            <div class="col-lg-12">
-                              <fieldset>
-                                <textarea name="message" rows="6" id="message" placeholder="Message" required=""></textarea>
-                              </fieldset>
-                            </div>
-                            <div class="col-lg-12">
-                              <fieldset>
-                                <button type="submit" id="form-submit" class="main-button">Send Message</button>
-                              </fieldset>
-                            </div>
-                          </div>
-                        </form>
-                    </div>
+            </div>
+            <div class="col-lg-6 col-md-6 col-xs-12">
+                <div class="contact-form section-bg" style="background-image: url(assets/images/DOCWR.jpg)">
+                    <form id="contact" action="" method="post">
+                      <div class="row">
+                        <div class="col-md-6 col-sm-12">
+                          <fieldset>
+                            <input name="name" type="text" id="name" placeholder="Your Name*" required="">
+                          </fieldset>
+                        </div>
+                        <div class="col-md-6 col-sm-12">
+                          <fieldset>
+                            <input name="email" type="text" id="email" pattern="[^ @]*@[^ @]*" placeholder="Your Email*" required="">
+                          </fieldset>
+                        </div>
+                        <div class="col-md-12 col-sm-12">
+                          <fieldset>
+                            <input name="subject" type="text" id="subject" placeholder="Subject">
+                          </fieldset>
+                        </div>
+                        <div class="col-lg-12">
+                          <fieldset>
+                            <textarea name="message" rows="6" id="message" placeholder="Message" required=""></textarea>
+                          </fieldset>
+                        </div>
+                        <div class="col-lg-12">
+                          <fieldset>
+                            <button type="submit" id="form-submit" class="main-button">Send Message</button>
+                          </fieldset>
+                        </div>
+                      </div>
+                    </form>
                 </div>
             </div>
         </div>
-    </section>
+    </div>
+</section>
     <!-- ***** Contact Us Area Ends ***** -->
     
     <!-- FOOTER -->
