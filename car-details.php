dbe19883055e188727b0335aa94c74ab4f6da14a
Daileyz <501936@vistacollege.nl>
2021-04-14 23:12:38 +0200

    upd

--- car-details.php
+++ car-details.php
@@ -158,9 +158,9 @@ require_once("assets/languages/lang.$lang.php")
                        
 
                        <div class="col-sm-6">
-                            <label><?php echo BOUWJAAR; ?></label>
+                           
                        
-                            <p><?php echo $row['bouwjaar'];?></p>
+                           
                        </div>
                        <div class="col-sm-6">
                             <label><?php echo MODEL; ?></label>
@@ -174,29 +174,29 @@ require_once("assets/languages/lang.$lang.php")
                   <article id='tabs-2'>
                     <h4>Job description</h4>
                     
-                    <p>- Colour coded bumpers <br> - Tinted glass <br> - Immobiliser <br> - Central locking - remote <br> - Passenger airbag <br> - Electric windows <br> - Rear head rests <br> - Radio <br> - CD player <br> - Ideal first car <br> - Warranty <br> - High level brake light <br> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco                         laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat                     cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p> 
+                    <p>- Vernieuwen van de website <br> - zorgen dat het systeem goed werkt<br> - . <br> - . <br> - . <br> - . <br> - . <br> - . <br> - . <br> - . <br> - . <br> - . <br> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco                         laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat                     cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p> 
                    </article>
                   <article id='tabs-3'>
                     <h4> Extras</h4>
 
                     <div class="row">   
                         <div class="col-sm-6">
-                            <p>ABS</p>
+                            <p>- Collega uitjes</p>
                         </div>
                         <div class="col-sm-6">
-                            <p>Leather seats</p>
+                            <p>- Tijdens feestdagen leuke activiteiten met het bedrijf</p>
                         </div>
                         <div class="col-sm-6">
-                            <p>Power Assisted Steering</p>
+                            <p>- Goede werkomgeving</p>
                         </div>
                         <div class="col-sm-6">
-                            <p>Electric heated seats</p>
+                            <p></p>
                         </div>
                         <div class="col-sm-6">
-                            <p>New HU and AU</p>
+                            <p></p>
                         </div>
                         <div class="col-sm-6">
-                            <p>Xenon headlights</p>
+                            <p></p>
                         </div>
                     </div>
                   </article>
@@ -208,7 +208,16 @@ require_once("assets/languages/lang.$lang.php")
                             <label>Names</label>
 
                             <p>
-                            Luca Lardenoije
+                            Luca L
+                            <br>
+                            Dailey 
+                            <br>
+                            raphael
+                            <br>
+                            raf 
+                            <br>
+                            Luca H
+                            
                             </p>
                         </div>
                         <div class="col-sm-6">
